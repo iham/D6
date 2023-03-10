a0f679530c1a17b18c526d778df18f29188dfc9e
Markus Hilbert <markus.hilbert@iham.at>
2023-03-10 16:42:52 +0000

    init node project.

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, '../index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
