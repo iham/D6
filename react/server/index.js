33a18cae330a91f0aa3de0e7401e91930df9b5ad
Markus Hilbert <markus.hilbert@iham.at>
2023-03-10 17:38:04 +0000

    moved to separate node project folders inside one container (for test purpose only!)

--- react/server/index.js
+++ react/server/index.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const path = require("path");
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 const app = express();
 
 app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
-});
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, '../index.html'));
+  res.json({ message: "Hello from react server!" });
 });
 
 app.listen(PORT, () => {
